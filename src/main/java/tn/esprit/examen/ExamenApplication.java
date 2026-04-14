c711f3f21fb129138e93246329d5a02ea8c5b7f9
syrineklai8 <syrine.klai@esprit.tn>
2026-04-14 11:37:50 +0200

    test3 succes

--- src/main/java/tn/esprit/examen/ExamenApplication.java
+++ src/main/java/tn/esprit/examen/ExamenApplication.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ExamenApplication {
 
 	public static void main(String[] args) {
-//   SpringApplication.run(ExamenApplication.class, args);
+SpringApplication.run(ExamenApplication.class, args);
 	}
 
 }
